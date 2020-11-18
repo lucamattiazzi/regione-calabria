56c56571c1963d6a14e521a4cf15afeb9c950546
Luca Mattiazzi <l.d.mattiazzi@gmail.com>
2020-11-18 02:38:17 +0100

    fixed email

--- src/mail.ts
+++ src/mail.ts
@@ -1,19 +1,22 @@
-// import sgMail from '@sendgrid/mail'
-import mailgun from 'mailgun-js'
+import FormData from 'form-data'
 import { UserData } from './types'
 import { capitalize } from 'lodash'
 import { config } from 'dotenv'
-import got from 'got'
+const mailgun = require('mailgun.js')
 
 config()
 
-const mg = mailgun({ apiKey: process.env.MAILGUN_TOKEN, domain: process.env.MAILGUN_DOMAIN })
+const { MAILGUN_TOKEN, MAILGUN_DOMAIN } = process.env
 
-// sgMail.setApiKey(process.env.SENDGRID_TOKEN)
+const mg = mailgun.client({
+  username: 'api',
+  key: MAILGUN_TOKEN,
+  url: 'https://api.eu.mailgun.net',
+})
 
 export function sendMail(userData: UserData) {
   const body = {
-    to: userData.email,
+    to: `${userData.email}, io@regionecalabria.itaila.it`,
     from: 'ViceCommissario Sanità Regione Calabria<sanita@regionecalabria.itaila.it>',
     subject: 'Nomina al ruolo di commissariə Sanità Regione Calabria',
     html: `<img src="https://portale.regione.calabria.it/website/images/lungohome.jpg" href="https://regionecalabria.itaila.it/sanita.html?uuid=${
@@ -32,31 +35,10 @@ export function sendMail(userData: UserData) {
       </p>
       `,
   }
+  const formData = new FormData()
+  for (const key in body) {
+    formData.append(key, body[key])
+  }
 
-  // got.post(process.env.MAILGUN_DOMAIN, {
-  //   headers: {
-  //     Authorization: 'Basic ' + Buffer.from(`'api:${process.env.MAILGUN_TOKEN}`).toString('base64'),
-  //   },
-  //   body,
-  // })
-
-  mg.messages().send(body).then(console.log)
+  mg.messages.create(MAILGUN_DOMAIN, body).then(console.log).catch(console.log)
 }
-
-sendMail({
-  email: 'tanke88@gmail.com',
-  firstName: 'luca',
-  lastName: 'mattiazzi',
-  message: 'stocazzo',
-})
-
-// const data = {
-//   from: 'Excited User <me@samples.mailgun.org>',
-//   to: 'tanke88@gmail.com',
-//   subject: 'Hello',
-//   text: 'Testing some Mailgun awesomness!',
-// }
-// mg.messages().send(data, function (error, body) {
-//   console.log(error)
-//   console.log(body)
-// })
